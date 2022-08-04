62c5c53296c2147cafcec2f2dc96a55438fe2c47
Tomas Pulenta <tpulenta@gmail.com>
2022-08-04 12:09:52 -0300

    feat: adding basic styles and texts

--- infinite-hackathon/src/App.js
+++ infinite-hackathon/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import infinite from './infinite.svg'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="info-text title-text">
+        <img src={infinite} alt="" className="title-image" />
+        <br />
+        <strong className='float-left'>GENESIS EDITION</strong>
+        <strong className="float-right"> #0 </strong>
+      </div>
+      <div className="item">
+        <div className="info-text"><span className="dot"></span> <strong>Date</strong> 7-9 October 2022</div>
+        <div className="info-text"><span className="dot"></span> <strong>Location</strong> Bogota Colombia </div>
+      </div>
+      <div className="info-text">
+        <span class="dot"/> <strong>Infinite Genesis </strong>
+          is the inaugural edition of a more intimate hackathon built for <strong>hackers first</strong>.
+          <br />Specializing in DeFi and DAOs, hackers will have 48 hours to realize their wildest concepts in a <strong> comfortable and accommodating environment</strong>.
+          <br />Projects will compete for a variety of prizes, including Infinite <strong>trophies, medals, NFTs and crypto</strong>!
+          <br />The judging will be public and streamed live.
+      </div>
     </div>
   );
 }
