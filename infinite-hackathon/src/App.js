a8cd9e342fd074babaf9abbf3434ce6e471ea621
Tomas Pulenta <tpulenta@gmail.com>
2022-08-05 16:09:54 -0300

    feat(wouter, components and styles): adding wouter, separating in components and stylesheets

--- infinite-hackathon/src/App.js
+++ infinite-hackathon/src/App.js
@@ -1,27 +1,24 @@
-import infinite from './infinite.svg'
-
+import { Route } from 'wouter'
+import Faq from './pages/Faq';
+import Home from './pages/Home';
 import './App.css';
+
+import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <div className="info-text title-text">
-        <img src={infinite} alt="" className="title-image" />
-        <br />
-        <strong className='float-left'>GENESIS EDITION</strong>
-        <strong className="float-right"> #0 </strong>
-      </div>
-      <div className="item">
-        <div className="info-text"><span className="dot"></span> <strong>Date</strong> 7-9 October 2022</div>
-        <div className="info-text"><span className="dot"></span> <strong>Location</strong> Bogota Colombia </div>
-      </div>
-      <div className="info-text">
-        <span class="dot"/> <strong>Infinite Genesis </strong>
-          is the inaugural edition of a more intimate hackathon built for <strong>hackers first</strong>.
-          <br />Specializing in DeFi and DAOs, hackers will have 48 hours to realize their wildest concepts in a <strong> comfortable and accommodating environment</strong>.
-          <br />Projects will compete for a variety of prizes, including Infinite <strong>trophies, medals, NFTs and crypto</strong>!
-          <br />The judging will be public and streamed live.
-      </div>
+      <Header/>
+      <Route
+        component={Home}
+        path='/'
+      />
+      <Route
+        component={Faq}
+        path='/faq'
+      />
+      <Footer/>
     </div>
   );
 }
