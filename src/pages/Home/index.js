4bc9e6c0377ba14d8a6e13553e26f67b056ff641
Tomas Pulenta <tpulenta@gmail.com>
2022-09-07 08:40:55 -0300

    style(burger-button): fix on burger-button img and remove unnecesary code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,10 +4,8 @@ import '../../styles/Home.css';
 import Organizers from '../../components/Organizers';
 import InfinitePhrases from '../../components/InfinitePhrases';
 import Drop from '../../components/Drop';
-import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Button from '../../components/Button';
-import Social from '../../components/Social';
 
 export default function Home() {
   return (
